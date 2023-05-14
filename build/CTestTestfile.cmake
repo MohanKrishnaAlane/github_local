830ce1983d97fd565954513a9d82a073e53cdbed
malane <mohan.alane@gmail.com>
2023-05-14 10:40:25 +0530

    Google test framework

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/github_local/Example
+# Build directory: D:/github_local/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googletest")
+subdirs("test")
